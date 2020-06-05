1a51e9448418acedc640f1116925e94bf7964dc9
Becky Rosa <becky_m_rosa@yahoo.com>
2020-06-05 03:24:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   create_an_array = ["I", "am", "really", "learning"]
 end
 
-def add_element_to_end_of_array
+def add_element_to_end_of_array(create_an_array)
   create_an_array.push "arrays!"
 end
 
