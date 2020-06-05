70c86c3c7a98c215ecd3dcbc27f42e8ab94b2497
Becky Rosa <becky_m_rosa@yahoo.com>
2020-06-05 03:53:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,6 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
+    create_an_array = ["wow", "I", "am", "really", "learning"]
   create_an_array.shift
 end
 
