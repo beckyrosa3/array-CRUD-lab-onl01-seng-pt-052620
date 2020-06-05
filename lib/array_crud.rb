78e0053dfae3ea99ac5bc8ab591cd2f694451bdf
Becky Rosa <becky_m_rosa@yahoo.com>
2020-06-05 03:13:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,15 +4,15 @@ end
 
 def create_an_array
   create_an_array = ["I", "am", "really", "learning"]
+end
 
+def add_element_to_end_of_array
   create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
-end
 
-def remove_element_from_end_of_array(array)
   create_an_array.pop
 end
 
